ab1bac6d15cd2f6df2790a26f548ac283e2e8617
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2025-02-16 08:01:03 -0500

    Update d3SVG to not use React ref

--- src/d3SVG.ts
+++ src/d3SVG.ts
@@ -1,17 +1,19 @@
-import { select } from 'd3';
+import { create } from "d3";
 
-const d3SVG = (svgRefCurrent: any, data: any) => {
-  svgRefCurrent.innerHTML = "";
-  const svg: any = select(svgRefCurrent)
-  svg.append('rect')
-    .attr('width', '200')
-    .attr('height', '200')
-    .attr('fill', 'red')
-  svg.append('text')
-    .attr('text-anchor', 'start')
-    .attr('x', 100)
-    .attr('y', 100)
-    .text(data)
-}
+const d3SVG = () => {
+  const svg = create("svg");
+  svg
+    .append("rect")
+    .attr("width", "200")
+    .attr("height", "200")
+    .attr("fill", "red");
+  svg
+    .append("text")
+    .attr("text-anchor", "start")
+    .attr("x", 100)
+    .attr("y", 100)
+    .text(data);
+  return svg.node();
+};
 
 export default d3SVG;
