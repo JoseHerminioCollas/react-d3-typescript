2f1cc4bff227afda07d7a3726ce15e1a00247de8
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2025-02-16 11:15:35 -0500

    Add global definitions to satisfy TypeScript

---
+++ globals.d.ts
@@ -0,0 +1,4 @@
+declare var test: (title: string, fn: () => void) => void;
+declare var describe: (title: string, fn: () => void) => void;
+declare var it: (title: string, fn: () => void) => void;
+declare var expect: (value: any) => any;
