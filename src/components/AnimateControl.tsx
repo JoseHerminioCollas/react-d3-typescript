67472363d70cb61c00695a039de2a740155eb679
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2025-02-16 10:43:03 -0500

    Refactor AnimateControl

--- src/components/AnimateControl.tsx
+++ src/components/AnimateControl.tsx
@@ -1,12 +1,29 @@
-import React from "react";
+/**
+ * @fileoverview Receive React state variables for control
+ *
+ * @param {boolean} animationOn
+ * @param {(value: boolean) => void} setAnimationOn
+ * @returns {JSX.Element}
+ */
 
-const AnimateControl = ({ animationOn, setAnimationOn }: any) => {
+type AnimateControlProps = {
+  animationOn: boolean;
+  setAnimationOn: (value: boolean) => void;
+};
+
+const text = { on: "Animation ON", off: "Animation OFF" };
+
+const AnimateControl: React.FC<AnimateControlProps> = ({
+  animationOn,
+  setAnimationOn,
+}) => {
   return (
     <section>
-      Animate
-      {!animationOn && <button onClick={() => setAnimationOn(true)}>ON</button>}
+      {!animationOn && (
+        <button onClick={() => setAnimationOn(true)}>{text.on}</button>
+      )}
       {animationOn && (
-        <button onClick={() => setAnimationOn(false)}>OFF</button>
+        <button onClick={() => setAnimationOn(false)}>{text.off}</button>
       )}
     </section>
   );
